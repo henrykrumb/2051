457bce0dae037fbb0cfbfa20a7da902cfda48db6
Henry Krumb <mail@henrykrumb.de>
2021-12-03 18:47:02 +0100

    include resources folder during installation

--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ setup(
     author='Henry Krumb',
     author_email='mail@henrykrumb.de',
     url='',
+    include_package_data=True,
+    package_data={
+        'twentyfiftyone': [
+            'resources/*',
+            'resources/assets/*/*',
+            'resources/fonts/*',
+            'resources/rooms/*',
+        ]
+    },
     packages=['twentyfiftyone'],
     entry_points='''
         [console_scripts]
